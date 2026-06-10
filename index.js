164df5281af995e77b81808d6b6cbf61f5c80c26
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-10 18:30:43 +0530

    add .env file, nodemon

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+let myusername = process.env.MY_USERNAME;
+
+console.log("value: ", myusername);
+
 console.log("Start of backend project");
-const name = "Vaibhav";
-
-const hello = () => {
-  console.log(name);
-};
-
-const city = "Bhopal";
