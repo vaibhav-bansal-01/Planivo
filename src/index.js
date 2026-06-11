59e3454ba6dab163dddebc6cf0845effcad8d345
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-11 19:17:16 +0530

    MongoDB connection establish

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import dotenv from "dotenv";
 
 import app from "./app.js";
 
+import connectDB from "./db/index.js";
+
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port http:localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  });
