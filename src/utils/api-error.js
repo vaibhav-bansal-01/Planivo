d397312c8a54b2d02940bfb9e7472043060cee0b
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-11 18:29:04 +0530

    add api-response.js ans api-error.js

---
+++ src/utils/api-error.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    messgae = "Something went wrong",
+    errors = [],
+    stack = "", //stack means stack trace — a record of where an error happened and the chain of function calls that led to it. Mainly used for debugging
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
