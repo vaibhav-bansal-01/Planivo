0b794bd580980b5151c52d9ff60378887c11b357
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-13 20:00:27 +0530

    add forgot password and email verification mailgen

---
+++ src/utils/mail.js
@@ -0,0 +1,43 @@
+import Mailgen from "mailgen";
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! We are are existed to have you on board",
+      action: {
+        instructions:
+          "To verify your email please click on the following button",
+        button: {
+          color: "#22BC66",
+          text: "Verify this email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we would love to help",
+    },
+  };
+};
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got the request to reset the password of your account",
+      action: {
+        instructions:
+          "To reset your password click on the following button or link",
+        button: {
+          color: "#22BC66",
+          text: "Reset password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we would love to help",
+    },
+  };
+};
+
+export { emailVerificationMailgenContent, forgotPasswordMailgenContent };
