6f36ef8bce00f95a0a66dad2021cfc8fa753ad8a
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-13 17:36:10 +0530

    add healthcheck controllers and routes

--- src/app.js
+++ src/app.js
@@ -23,6 +23,11 @@ app.use(
   }),
 );
 
+// import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome to baseCamp");
 });
