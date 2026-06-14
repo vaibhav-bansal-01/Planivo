89fc027b7a96235c783195c3b7fca622528e7ab7
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-14 15:42:19 +0530

    add validations and middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
+import { validate } from "../middlewares/validators.middleware.js";
+import { userRegisterValidator } from "../validators/index.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
 
 export default router;
