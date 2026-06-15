82ab0be59e29965e8d456aa81d9e7fb0c4e8a516
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-06-15 16:42:41 +0530

    add schemas

---
+++ src/models/projectmember.models.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableRoles, UserRolesEnum } from "../utils/constants.js";
+
+const projectMemberSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: AvailableRoles,
+      default: UserRolesEnum.MEMBER,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Projectmember = mongoose.model(
+  "Projectmember",
+  projectMemberSchema,
+);
---
+++ src/models/task.models.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatus, TaskStatusEnum } from "../utils/constants.js";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: String,
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: AvailableTaskStatus,
+      default: TaskStatusEnum.TODO,
+    },
+    attachments: {
+      type: [
+        {
+          url: string,
+          mimeType: string,
+          size: Number,
+        },
+      ],
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+export const Task = mongoose.model("Task", taskSchema);
