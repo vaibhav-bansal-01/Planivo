802ad8025bfd492447680b5972a32affaecb3ed6
Vaibhav Bansal <vaibhavb11104@gmail.com>
2026-07-04 22:43:17 +0530

    add changes

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -25,7 +25,7 @@ const generateJWT = async (userId) => {
 };
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { email, username, password, role } = req.body;
+  const { email, username, password } = req.body;
 
   const existedUser = await User.findOne({
     $or: [{ username }, { email }],
@@ -59,7 +59,7 @@ const registerUser = asyncHandler(async (req, res) => {
     subject: "Please verify your email",
     mailgenContent: emailVerificationMailgenContent(
       user.username,
-      `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`,
+      `${process.env.EMAIL_VERIFICATION_REDIRECT_URL}/${unHashedToken}`,
     ),
   });
 
